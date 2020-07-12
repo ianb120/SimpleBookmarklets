29ae320bb1f2b922c339d643397c616f52fd9558
Ian (The Test Guy) <37348701+ianb120@users.noreply.github.com>
2020-07-12 12:58:45 +0100

    Update PageIdValue.js
    Bookmarklet that returns a hardcoded page id value

--- PageIdValue.js
+++ PageIdValue.js
@@ -1,1 +1,2 @@
+// returns the page id value as an alert, this value is hardcoded and is constantly being checked serveral times a week 
 javascript: alert(document.getElementById('__VIEWSTATE').value);

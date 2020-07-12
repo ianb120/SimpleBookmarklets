f5c5a38f89791768ae159295d514fc8f12a19c91
Ian (The Test Guy) <37348701+ianb120@users.noreply.github.com>
2020-07-12 12:36:00 +0100

    Update ClickToHighlightElement.js

--- ClickToHighlightElement.js
+++ ClickToHighlightElement.js
@@ -1,1 +1,2 @@
+// with every click event that is registered in the browser, the clicked element will be highlighted with a green border.
 javascript:document.addEventListener('click',(event) => {let getCurrentElement = event.target; getCurrentElement.style.border = '5px solid #86C232';});
